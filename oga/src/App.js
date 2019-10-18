e98b43dcfb00141c2850e6fbd6f12219c255aca9
thoum <indiofish@gmail.com>
2019-10-19 00:15:04 +0900

    add path for map

--- oga/src/App.js
+++ oga/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Map from './containers/Map/GoogleMap';
+import {connect} from 'react-redux';
+import {Route, Redirect, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+ return (
+  <ConnectedRouter history={props.history}>
+    <div className='App'>
+      <Switch>
+        <Route path='/map' exact component={Map}/>
+      </Switch>
     </div>
-  );
+  </ConnectedRouter>
+ );
 }
-
 export default App;
