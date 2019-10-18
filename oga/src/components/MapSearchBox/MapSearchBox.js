93812700fb147178331f0d58db23822cebd4c9c4
thoum <5348027+taehioum@users.noreply.github.com>
2019-10-19 00:56:00 +0900

    Feature mappage (#1)
    * add googlemaps package
    
    * use better googlemapsapi
    
    * add MapSearchBox Component
    
    * add a basic map component
    
    * connect with store for location submit
    
    * add directory for constant values
    
    * add settargetlocation functionalities
    
    * remove unused packages
    
    * add yarn.lock
    
    * add path for map
    
    * use <Provider>
    
    * use arrow functions instead of bind(this)

---
+++ oga/src/components/MapSearchBox/MapSearchBox.js
@@ -0,0 +1,58 @@
+/**
+ * Summary.
+ * MapSearchBox for Maps, using google maps API.
+ * Referred from google-map-react examples
+ *
+ * Description.
+ * MapSearchBox component that provides recommendations near the map location.
+ * @author taehioum
+ * @since  2019-10-18
+ */
+ 
+/** jshint {inline configuration here} */
+import React, { Component } from 'react';
+import './MapSearchBox.css';
+
+class MapSearchBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount({ map, mapApi } = this.props) {
+    this.searchBox = new mapApi.places.SearchBox(this.searchInput);
+    this.searchBox.addListener('places_changed', this.onPlacesChanged);
+    this.searchBox.bindTo('bounds', map);
+  }
+
+  componentWillUnmount({ mapApi } = this.props) {
+    mapApi.event.clearInstanceListeners(this.searchInput);
+  }
+
+  onPlacesChanged = ({ map, addplace } = this.props) => {
+    const selected = this.searchBox.getPlaces();
+    const { 0: place } = selected;
+    addplace(selected);
+    this.searchInput.blur();
+  };
+
+  clearSearchBox = () => {
+    this.searchInput.value = '';
+  };
+
+  render() {
+    return (
+      <div className="MapSearchBox">
+        <input
+          ref={(ref) => {
+            this.searchInput = ref;
+          }}
+          type="text"
+          onFocus={this.clearSearchBox}
+          placeholder="Enter a location"
+        />
+      </div>
+    );
+  }
+}
+
+export default MapSearchBox;
---
+++ oga/src/const/api_key.js
@@ -0,0 +1,1 @@
+export default 'AIzaSyCGZwN3SLxLWUoSN3Mcf3B19QXtvd6yCPQ';
