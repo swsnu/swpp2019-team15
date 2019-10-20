99db3688ab848d6740665ddda0a5f42fa829c786
thoum <indiofish@gmail.com>
2019-10-20 20:50:47 +0900

    add api urls

--- oga/backend/users/urls.py
+++ oga/backend/users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('questions/', views.questions, name='questions'),
     path('<str:username>/', views.UserProfile, name='user_profile'),
     path('<str:username>/main/', views.Main, name='user_main'),
     path('<int:question_id>/detail/', views.Details, name='details'),
