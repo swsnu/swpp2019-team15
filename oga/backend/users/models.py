1b025870eaccf19dc3146c4b03329ef19866a6e6
thoum <indiofish@gmail.com>
2019-10-20 19:20:48 +0900

    location is not a mandatory key for users

--- oga/backend/users/models.py
+++ oga/backend/users/models.py
@@ -8,12 +8,14 @@ class Location(models.Model):
     
     def __str__(self):
         return self.name
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20) 
     # can use Pointfield to store location coordinates
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE,
+            blank=True, null=True)
 
     def __str__(self):
         return self.username
