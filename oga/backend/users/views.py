8efc34bf4d8ca8b143febce5e1ddbe13784df187
thoum <indiofish@gmail.com>
2019-10-20 19:51:37 +0900

    add question with mock author and location

--- oga/backend/users/views.py
+++ oga/backend/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import User, Question, Answer
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import User, Question, Answer, Location
 from django.views import generic
 
 
@@ -22,4 +24,24 @@ def Details(request, question_id):
     # questions to answers are one to many
     answer_list = [answer for answer in Answer.objects.filter(question = question_to_answer).values()]
     return render(request, 'users/detail.html', {'question': question_to_answer, 'answer_list': answer_list})
-    
+
+@csrf_exempt
+def questions(request):
+    if request.method == 'POST':
+        try:
+            body = request.body.decode()
+            # username = json.loads(body)['name']
+            # location = json.loads(body)['location']
+            content = json.loads(body)['content']
+            print(body)
+        except (KeyError, json.JSONDecodeError):
+            return HttpResponseBadRequest()
+        # FIXME: should use real data from body
+        user = get_object_or_404(User, username="hiboy")
+        location = get_object_or_404(Location, name="ome")
+        print(location)
+        question = Question(author=user, location_id=location,
+                            content=content)
+        question.save()
+        response_dict = {'id': question.id}
+        return JsonResponse(response_dict, status=201)
