115db8494520fe8a399567cc6a76c35b38e6a878
thoum <indiofish@gmail.com>
2019-10-20 23:14:14 +0900

    add signup api

--- oga/backend/users/urls.py
+++ oga/backend/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('questions/', views.questions, name='questions'),
+    path('signup/', views.sign_up, name='sign_up'),
     path('<str:username>/', views.UserProfile, name='user_profile'),
     path('<str:username>/main/', views.Main, name='user_main'),
     path('<int:question_id>/detail/', views.Details, name='details'),
