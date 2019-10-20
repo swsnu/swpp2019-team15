15f550120775e06264954b5b7f43071f78592035
thoum <indiofish@gmail.com>
2019-10-20 20:45:36 +0900

    import Locations

--- oga/backend/users/admin.py
+++ oga/backend/users/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import User, Question, Answer
+from .models import User, Question, Answer,Location
 
 admin.register(User, Question, Answer, Location)(admin.ModelAdmin)
