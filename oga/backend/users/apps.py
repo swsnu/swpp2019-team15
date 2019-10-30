de51e47c1214bb6cc59fc46b6a7bf96698d640e0
Minji1234 <mmra12345@snu.ac.kr>
2019-10-30 19:52:59 +0900

    added other files

--- oga/backend/users/apps.py
+++ oga/backend/users/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as __init__(self, *args, **kwargs):
 
 
 class UsersConfig(AppConfig):
     name = 'users'
+    verbose_name = _('users')
+
+    def ready(self):
+        import users.signals
