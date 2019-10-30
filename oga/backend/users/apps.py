a50d2843ee445593939fdd2e2b56d5f38e2d7d83
Minji1234 <mmra12345@snu.ac.kr>
2019-10-30 20:02:21 +0900

    temp

--- oga/backend/users/apps.py
+++ oga/backend/users/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as __init__(self, *args, **kwargs):
+from django.utils.translation import ugettext_lazy as _
 
 
 class UsersConfig(AppConfig):
