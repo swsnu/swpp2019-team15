306ab74b6575377813db07ce62b1dba94d4033b0
thoum <5348027+taehioum@users.noreply.github.com>
2019-11-01 02:20:22 +0900

    clean up code: removed unused imports, and functions (#44)

--- oga/backend/users/signals.py
+++ oga/backend/users/signals.py
@@ -1,12 +1,16 @@
-from .models import User, Profile
+"""signals and appropriate actions"""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .models import User, Profile
 
+# pylint: disable=unused-argument
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """called upon user creation"""
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    """called upon user update / save"""
     instance.profile.save()
