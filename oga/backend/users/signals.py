b2b901bbee9d13053f5bf6ebf5e66e0c8c29110a
thoum <indiofish@gmail.com>
2019-11-01 02:42:05 +0900

    add skeleton

--- oga/backend/users/signals.py
+++ oga/backend/users/signals.py
@@ -1,7 +1,8 @@
 """signals and appropriate actions"""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, Profile
+from .models import User, Profile, Question
+from .utils.send_push import send_push
 
 # pylint: disable=unused-argument
 @receiver(post_save, sender=User)
@@ -14,3 +15,14 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     """called upon user update / save"""
     instance.profile.save()
+
+
+@receiver(post_save, sender=Question)
+def notify_new_question(sender, instance, created, **kwargs):
+    """upon saving a new question, find relevant users and send push
+    notificaiton"""
+    if created:
+        profiles = Profile.objects.all()
+        for profile in profiles:
+            if instance.author != profile.user:
+                send_push(profile, {"text": "newquestion!", "tag": "question"})
