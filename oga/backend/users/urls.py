066f52879d500eed77c9fe026ed63ab055cdc6fa
thoum <indiofish@gmail.com>
2019-11-01 19:43:19 +0900

    add location api

--- oga/backend/users/urls.py
+++ oga/backend/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('questions/', views.questions, name='questions'),
     path('signup/', views.sign_up, name='sign_up'),
     path('signin/', views.sign_in, name='sign_in'),
-    path('location/', views.set_location, name='set_location'),
+    path('location/', views.locations, name='set_location'),
 ]
--- oga/backend/users/views/views_location.py
+++ oga/backend/users/views/views_location.py
@@ -1,6 +1,10 @@
 """location related views"""
+import json
 from django.contrib.auth import get_user
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from users.views.decorators import check_request, check_login_required
 from users.models import Location
 
@@ -11,10 +15,10 @@ from users.models import Location
 def locations(request):
     """on post, store user's location"""
     req_data = json.loads(request.body.decode())
-    location = req_data['location']
-    location, _ = Location.objects.get_or_create(name=location['name'],
-                                                 latitude=location['latitude'],
-                                                 longitude=location['longitude'])
+    print(req_data)
+    location, _ = Location.objects.get_or_create(name=req_data['name'],
+                                                 latitude=req_data['latitude'],
+                                                 longitude=req_data['longitude'])
     user = get_user(request)
     user.location = location
     user.save()
