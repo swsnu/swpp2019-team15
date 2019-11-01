40576cb7f5f1f640e73ff80b8941caac00b94be9
Minji1234 <mmra12345@snu.ac.kr>
2019-11-01 14:06:13 +0900

    implemented

--- oga/backend/users/signals.py
+++ oga/backend/users/signals.py
@@ -1,8 +1,9 @@
 """signals and appropriate actions"""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, Profile, Question
+from .models import User, Profile, Question, Answer, Location
 from .utils.send_push import send_push
+from .utils.haversine import haversine
 
 # pylint: disable=unused-argument
 @receiver(post_save, sender=User)
@@ -16,13 +17,25 @@ def save_user_profile(sender, instance, **kwargs):
     """called upon user update / save"""
     instance.profile.save()
 
-
 @receiver(post_save, sender=Question)
 def notify_new_question(sender, instance, created, **kwargs):
     """upon saving a new question, find relevant users and send push
     notificaiton"""
     if created:
+        qs_l = Location.objects.get(id=instance.location_id)
         profiles = Profile.objects.all()
         for profile in profiles:
+            rc_l = Location.objects.get(id=profile.location_id)
             if instance.author != profile.user:
-                send_push(instance.location_id, profile, {"text": "newquestion!", "tag": "question"})
+                if haversine(qs_l.longitude, qs_l.latitude, rc_l.longitude, rc_l.latitude) <= 0.3:
+                    send_push(profile, {"text": "newquestion!", "tag": "question"})
+
+
+@receiver(post_save, sender=Answer)
+def notify_new_answer(sender, instance, created, **kwargs):
+    """upon saving a new answer, find owner of question and send push
+    notificaiton"""
+    if created:
+        qs_sender_id = instance.question.author
+        profile = Profile.objects.get(user=qs_sender_id)
+        send_push(profile, {"text": "newAnswer!", "tag": "answer"})
--- oga/backend/users/utils/send_push.py
+++ oga/backend/users/utils/send_push.py
@@ -1,33 +1,8 @@
 """wrapper for webpush"""
 import json
 from pywebpush import webpush, WebPushException
-from users.models import Location
-from math import radians, cos, sin, asin, sqrt
 
-# haversine formula
-# from https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
-
-def check_sender_near_receiver(qs_loc, rc_loc):
-    distance = haversine(qs_loc.longitude, qs_loc.latitude, rc_loc.longitude, rc_loc.latitude)
-    # return true if distance from question location and receiver's location is lower than 300m
-    return (distance < 0.3)
-
-def send_push(question_location_id, profile, body):
+def send_push(profile, body):
     """
     given a subscription of a user (a Profile)
     send an appropriate notification with body (a dictionary object) notification
@@ -36,14 +11,11 @@ def send_push(question_location_id, profile, body):
     send_push(profile, {'title': 'hi', 'text': 'hi'})
     """
     try:
-        question_location = Location.objects.get(id=question_location_id)
-        receiver_location = Location.objects.get(id=profile.location_id)
-        if check_sender_near_receiver(question_location, receiver_location):
-            webpush(profile.subscription,
-                    json.dumps(body),
-                    # encoded as a hard string as of now
-                    vapid_private_key="LhJWR3cBwqckwjYMC1vQoCLXmI8d3qXK6LOUMZ-6LzY",
-                    vapid_claims={"sub": "mailto:indiofish@naver.com"})
+        webpush(profile.subscription,
+                json.dumps(body),
+                # encoded as a hard string as of now
+                vapid_private_key="LhJWR3cBwqckwjYMC1vQoCLXmI8d3qXK6LOUMZ-6LzY",
+                vapid_claims={"sub": "mailto:indiofish@naver.com"})
     except WebPushException as ex:
         print(": {}", repr(ex))
         # Mozilla returns additional information in the body of the response.
