97ccf421e7784483fae751203da49cc604cd5aeb
thoum <indiofish@gmail.com>
2019-11-02 23:31:51 +0900

    test for location Actions

--- oga/frontend/src/store/actions/locationActions.test.js
+++ oga/frontend/src/store/actions/locationActions.test.js
@@ -15,4 +15,44 @@ describe('authActions', () => {
     expect(newState.location.targetLocation).toStrictEqual({name:"home", latitude:10, longitude:10});
     done();
   });
+
+  it(`'setCurrentLocation' should set current location correctly`, (done) => {
+    const spy = jest.spyOn(axios, 'post')
+      .mockImplementation((url, coords) => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: {},
+          };
+          resolve(coords);
+        });
+      });
+
+    const newCoordinates = {latitude: 12, longitude:10};
+
+    store.dispatch(actionCreators.setCurrentCoordinates(newCoordinates)).then(() => {
+      const newState = store.getState();
+      expect(newState.location.currentCoordinates).toStrictEqual(newCoordinates);
+      done(); });
+  });
+
+  it(`'setCurrentLocation' should not change location upon error`, (done) => {
+    const oldState = store.getState();
+    const spy = jest.spyOn(axios, 'post')
+      .mockImplementation(url => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: {},
+          };
+          reject(result);
+        });
+      });
+    const newCoordinates = {latitude: 10, longitude:11};
+
+    store.dispatch(actionCreators.setCurrentCoordinates(newCoordinates)).then(() => {
+      const newState = store.getState();
+      expect(newState).toBe(oldState);
+      done(); });
+  });
 });
