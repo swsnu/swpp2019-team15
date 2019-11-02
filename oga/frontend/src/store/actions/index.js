8b0ab88b7d24c05b6c737fce620bdbcbb692c293
rachelg98 <37978535+rachelg98@users.noreply.github.com>
2019-11-02 22:13:29 +0900

    Feat/backend questionlist (#52)
    * Add backend views and main page
    
    * Add  is_answered field in question
    
    * remove duplicate import
    
    * clean imports
    
    * clean import
    
    * Add question view and tests
    
    * satisfy pylint

--- oga/frontend/src/store/actions/index.js
+++ oga/frontend/src/store/actions/index.js
@@ -1,9 +1,16 @@
-import {setTargetLocation, setCurrentCoordinates} from './locationActions.js';
-import {createQuestion} from './questionActions.js';
-import {signUp, signIn} from './authActions.js';
+import { setTargetLocation, setCurrentCoordinates } from "./locationActions.js";
+import {
+    createQuestion,
+    getQuestion,
+    getQuestions
+} from "./questionActions.js";
+import { signUp, signIn } from "./authActions.js";
 export {
-  setTargetLocation,
-  setCurrentCoordinates,
-  createQuestion,
-  signUp, signIn
-}
+    setTargetLocation,
+    setCurrentCoordinates,
+    createQuestion,
+    getQuestion,
+    getQuestions,
+    signUp,
+    signIn
+};
