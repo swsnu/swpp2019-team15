4cb385b067b03d0be5ccd8ece246a1d922333923
thoum <5348027+taehioum@users.noreply.github.com>
2019-11-03 12:11:43 +0900

    store location in profile, not user (#55)

--- oga/backend/users/views/views_location.py
+++ oga/backend/users/views/views_location.py
@@ -2,23 +2,25 @@
 import json
 from django.contrib.auth import get_user
 
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from users.views.decorators import check_request, check_login_required
-from users.models import Location
+from users.models import Location, Profile
 
 @check_login_required
 @check_request
 @require_http_methods(["POST"])
 def locations(request):
-    """on post, store user's location"""
+    """on post, store user's location in profile"""
     req_data = json.loads(request.body.decode())
     print(req_data)
     location, _ = Location.objects.get_or_create(name=req_data['name'],
                                                  latitude=req_data['latitude'],
                                                  longitude=req_data['longitude'])
     user = get_user(request)
-    user.location = location
-    user.save()
+    profile = get_object_or_404(Profile, user=user)
+    profile.location_id = location
+    profile.save()
     response_dict = {'success': True}
     return JsonResponse(response_dict, status=201)
