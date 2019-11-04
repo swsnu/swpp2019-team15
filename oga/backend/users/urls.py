a4511b4165841b63f4d76b6c724eacf59481aa3b
Minji1234 <mmra12345@snu.ac.kr>
2019-11-04 17:24:07 +0900

    integrity error found after pull

--- oga/backend/users/urls.py
+++ oga/backend/users/urls.py
@@ -11,13 +11,9 @@ urlpatterns = [
     path('is-authed/', views.is_logged_in, name='is_logged_in'),
     path('save-subscription/', views.save_subscription, name='save-subscription'),
     path('location/', views.locations, name='set_location'),
-<<<<<<< HEAD
     path('reply/<int:question_or_answer_id>/',
          views.get_or_create_answer, name='get_or_create_answer'),
-    path('replies/<int:question_id>/', views.get_answers, name='get_answers')
-=======
-    path('reply/<int:question_id>/', views.create_answer, name='create_answer'),
+    path('replies/<int:question_id>/', views.get_answers, name='get_answers'),
     path('follow/<int:question_id>/', views.follow_question,
          name='follow_question'),
->>>>>>> 17e178b5bdde50c3e861621a4816508b6013a352
 ]
