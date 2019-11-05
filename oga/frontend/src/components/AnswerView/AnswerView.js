4de2da5f246cac66a176879932d6dc7664aa1df2
Rachel <ganrachel98@gmail.com>
2019-11-06 03:03:38 +0900

    Fix minor typos

--- oga/frontend/src/components/AnswerView/AnswerView.js
+++ oga/frontend/src/components/AnswerView/AnswerView.js
@@ -3,25 +3,20 @@ import "./AnswerView.css";
 import moment from "moment";
 
 //Materials UI imports
-import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 
 const AnswerView = props => {
     return (
-        <div className="AnswerView">
-            {props.is_answered ? (
-                <Grid item key={props.key}>
-                    <Card className="Question">
+        <div className="AnswerView" key={props.key}>
+            <Grid item>
+                {props.is_answered ? (
+                    <Card>
                         <CardContent>
                             <Typography
                                 component="body"
-                                variant="outlined"
-                                borderStyle="solid"
-                                borderColor="primary"
                                 gutterBottom
                                 onClick={props.clickDetail}
                             >
@@ -45,12 +40,12 @@ const AnswerView = props => {
                             </Typography>
                         </CardContent>
                     </Card>
-                </Grid>
-            ) : (
-                <div>
-                    Is it {props.content} in {props.place_name}?
-                </div>
-            )}
+                ) : (
+                    <div>
+                        Is it {props.content} in {props.place_name}?
+                    </div>
+                )}
+            </Grid>
         </div>
     );
 };
--- oga/frontend/src/containers/Answer/NewAnswer.js
+++ oga/frontend/src/containers/Answer/NewAnswer.js
@@ -10,10 +10,8 @@ import { question_types } from "../../const/question_type";
 //Material design imports
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 
 class NewAnswer extends Component {
