695292c290572399490b2e801e838b21b2756750
thoum <indiofish@gmail.com>
2019-12-14 03:26:48 +0900

    satisfy linter

--- oga/backend/users/utils/recommender.py
+++ oga/backend/users/utils/recommender.py
@@ -1,20 +1,18 @@
 """wrapper for recombee"""
-import json
-from ..models import Location
-from background_task import background
 from recombee_api_client.api_client import RecombeeClient
-from recombee_api_client.exceptions import APIException
+# from recombee_api_client.exceptions import APIException
 from recombee_api_client.api_requests import AddPurchase, RecommendItemsToItem
-from recombee_api_client.api_requests import Batch, SetItemValues, ResetDatabase
-from recombee_api_client.api_requests import AddItemProperty
+from recombee_api_client.api_requests import Batch, SetItemValues
+from background_task import background
+from ..models import Location
 
-DB_NAME = 'askat-dev' 
+DB_NAME = 'askat-dev'
 API_KEY = 'LJ1SuCdQQsu2GnNt0eiQVOcT3kRK2nhDv8boWWdsTyheJSRMTWCbjfClmXfNy6nq'
-client = RecombeeClient(DB_NAME, API_KEY)
+CLIENT = RecombeeClient(DB_NAME, API_KEY)
 
 def get_recommendation(user, location):
     """return list of recommended place's names"""
-    recommended = client.send(RecommendItemsToItem(location, user, 2))
+    recommended = CLIENT.send(RecommendItemsToItem(location, user, 2))
     location_names = []
     for loc in recommended['recomms']:
         location = Location.objects.get(pk=loc['id'])
@@ -24,11 +22,11 @@ def get_recommendation(user, location):
 @background
 def add_item(user, location):
     """async add item to the recommbee DB"""
-    loc=Location.objects.get(pk=location)
+    loc = Location.objects.get(pk=location)
     request = []
     request.append(SetItemValues(location,
                                  {'lat': loc.latitude,
                                   'lng': loc.longitude},
                                  cascade_create=True))
     request.append(AddPurchase(user, location, cascade_create=True))
-    client.send(Batch(request))
+    CLIENT.send(Batch(request))
