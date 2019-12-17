6170671728a8a8237f57ab92340e37a84693360a
Minji1234 <mmra12345@snu.ac.kr>
2019-12-18 00:09:59 +0900

    WIP

--- oga/frontend/src/containers/Profile/Profile.js
+++ oga/frontend/src/containers/Profile/Profile.js
@@ -15,8 +15,7 @@ import {
     CardContent,
     CardMedia,
     Grid,
-    Typography,
-    ButtonGroup
+    Typography
 } from "@material-ui/core";
 
 class Profile extends Component {
@@ -58,17 +57,17 @@ class Profile extends Component {
 
     render() {
         let username = "";
-        let location = "";
-        let coordinates = "";
+        // let location = "";
+        // let coordinates = "";
         var todayQuestionCount = "";
         var todayAnswerCount = "";
         let reliability = "";
         var rankNum = 0;
         if (this.props.userProfile) {
             var profile = this.props.userProfile;
-            username = profile.username;
+            // username = profile.username;
             // location = `${profile.location}`;
-            // coordinates = `${profile.coordinates}`;
+            coordinates = `${profile.coordinates}`;
             todayQuestionCount = `${profile.todayQuestionCount}`;
             todayAnswerCount = `${profile.todayAnswerCount}`;
             reliability = `${profile.reliability}`;
